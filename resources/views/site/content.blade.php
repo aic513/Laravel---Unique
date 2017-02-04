536f547278300b7ee908b1f49bc465fc400562d7
aic513 <aic513@mail.ru>
2017-02-04 14:28:16 +0300

    Подправил секцию услуг

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -1,4 +1,3 @@
-
 @if(isset($pages) && is_object($pages))
 
     @foreach($pages as $key=>$page)
@@ -12,7 +11,8 @@
                                 <div class="col-lg-5 col-sm-7">
                                     <div class="top_left_cont zoomIn wow animated">
                                         {!! $page->text !!}
-                                        <a href="{{ route('page',array('alias'=>$page->alias))}}" class="read_more2">Read more</a> </div>
+                                        <a href="{{ route('page',array('alias'=>$page->alias))}}" class="read_more2">Read
+                                            more</a></div>
                                 </div>
                                 <div class="col-lg-7 col-sm-5">
                                     {!! Html::image('assets/img/'.$page->images) !!}
@@ -35,7 +35,9 @@
                                     <div class=" delay-01s animated fadeInDown wow animated">
                                         {!! $page->text !!}
                                     </div>
-                                    <div class="work_bottom"> <span>Want to know more..</span> <a href="{{ route('page',array('alias'=>$page->alias))}}" class="contact_btn">Contact Us</a> </div>
+                                    <div class="work_bottom"><span>Want to know more..</span> <a
+                                                href="{{ route('page',array('alias'=>$page->alias))}}"
+                                                class="contact_btn">Contact Us</a></div>
                                 </div>
 
                             </div>
@@ -47,72 +49,44 @@
             </section>
             <!--Aboutus-->
         @endif
-
     @endforeach
-
-
-
-
 @endif
 
 
 
+@if(isset($services) && is_object($services))
+    <!--Service-->
+    <section  id="service">
+        <div class="container">
+            <h2>Services</h2>
+            <div class="service_wrapper">
 
-<!--Service-->
-<section  id="service">
-    <div class="container">
-        <h2>Services</h2>
-        <div class="service_wrapper">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="service_block">
-                        <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-android"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">Android</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 borderLeft">
-                    <div class="service_block">
-                        <div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-apple"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">Apple IOS</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 borderLeft">
-                    <div class="service_block">
-                        <div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-html5"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">Design</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row borderTop">
-                <div class="col-lg-4 mrgTop">
-                    <div class="service_block">
-                        <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-dropbox"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">Concept</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 borderLeft mrgTop">
-                    <div class="service_block">
-                        <div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-slack"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">User Research</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 borderLeft mrgTop">
-                    <div class="service_block">
-                        <div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-users"></i></span> </div>
-                        <h3 class="animated fadeInUp wow">User Experience</h3>
-                        <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                    </div>
-                </div>
+                @foreach($services as $k=>$service)
+                    @if($k == 0 || $k%3 == 0)
+                        <div class="row {{ ($k != 0) ? 'borderTop' : '' }}">
+                            @endif
+
+                            <div class="col-lg-4 {{ ($k%3 > 0) ? 'borderLeft' : '' }} {{ ($k > 2) ? 'mrgTop' : ''}}">
+                                <div class="service_block">
+                                    <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa {{ $service->icon}}"></i></span> </div>
+                                    <h3 class="animated fadeInUp wow">{{ $service->name }}</h3>
+                                    <p class="animated fadeInDown wow">{{ $service->text }}</p>
+                                </div>
+                            </div>
+
+                            @if(($k + 1)%3 == 0)
+                        </div>
+                    @endif
+
+                @endforeach
+
+
             </div>
         </div>
-    </div>
-</section>
-<!--Service-->
+    </section>
+    <!--Service-->
+
+@endif
 
 
 

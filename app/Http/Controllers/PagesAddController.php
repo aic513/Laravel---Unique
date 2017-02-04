1fef0a00746e4f091874ee46a2be60ac7972c649
aic513 <aic513@mail.ru>
2017-02-04 23:44:59 +0300

    Сделал добавление новых страниц в бд

--- app/Http/Controllers/PagesAddController.php
+++ app/Http/Controllers/PagesAddController.php
@@ -3,15 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\Page;
 
 class PagesAddController extends Controller
 {
-    public function execute(Request $request){
-        if(view()->exists('admin.pages_add')) {
+    public function execute(Request $request)
+    {
+
+        if ($request->isMethod('post')) {
+            $input = $request->except('_token');   //записываем все,кроме  _token
+
+            $massages = [
+
+                'required' => 'Поле :attribute обязательно к заполнению',
+                'unique' => 'Поле :attribute должно быть уникальным'
+
+            ];
+
+
+            $validator = Validator::make($input, [
+
+                'name' => 'required|max:255',
+                'alias' => 'required|unique:pages|max:255',
+                'text' => 'required'
+
+            ], $massages);
+
+            if($validator->fails()) {    //проверка на валидацию
+                return redirect()->route('pagesAdd')->withErrors($validator)->withInput();
+            }
+
+            //для загрузки файлов
+            if($request->hasFile('images')) {
+                $file = $request->file('images');
+
+                $input['images'] = $file->getClientOriginalName();   //записываем оригинальное имя файла в ячейку массива
+
+                $file->move(public_path().'/assets/img',$input['images']);  //копируем файл в указанную папку
+
+            }
+
+            $page = new Page();   //создаем пустой объект модели Page
+
+
+            //$page->unguard();       //либо можем дать автоматический доступ ко всем поля
+
+            $page->fill($input);   //заполняем поля модели
+
+            if($page->save()) {
+                return redirect('admin')->with('status','Страница добавлена');
+            }
+
+
+        }
+
+
+        if (view()->exists('admin.pages_add')) {
             $data = [
                 'title' => 'Новая страница'
             ];
-            return view('admin.pages_add',$data);
+            return view('admin.pages_add', $data);
         }
         abort(404);
     }
